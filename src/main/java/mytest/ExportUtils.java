f942d6fd2646f8c039d27f5c172f309a7072184f
shuyi <shuyi>
2018-09-17 17:07:03 +0800

    第一次提交

---
+++ src/main/java/mytest/ExportUtils.java
@@ -0,0 +1,134 @@
+package mytest;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.collect.Lists;
+
+/**
+ * @author shuyi
+ * @date 2018/09/13
+ */
+public class ExportUtils {
+
+    /**
+     * 获取文件流
+     * 
+     * @param fileName
+     * @return
+     */
+    public static InputStream getFileInputStream(String fileName) {
+        InputStream inputStream = ExportUtils.class.getClassLoader().getResourceAsStream(fileName);
+        return inputStream;
+    }
+
+    /**
+     * 获取上级目录
+     * 
+     * @param path
+     * @return
+     */
+    public static String getParentPath(String path) {
+        return path.substring(0, path.lastIndexOf(IOUtils.DIR_SEPARATOR_UNIX));
+    }
+
+    /**
+     * 复制文件
+     */
+    public static void copyFile() {
+        File realFile = null;
+        File targetFile = null;
+        for (String filePath : FileTest.fileList) {
+            filePath = StringUtils.trim(filePath);
+            if (StringUtils.isEmpty(filePath) || StringUtils.startsWith(filePath, "#")) {
+                continue;
+            }
+            // 文件实际路径
+            String realPath = FileTest.WORKSPACE + filePath;
+            realFile = new File(realPath);
+            // 目标文件路径
+            String targetPath = FileTest.TARGET + filePath;
+            // 父目录
+            String parentPath = ExportUtils.getParentPath(realPath);
+            // 创建目标文件夹
+            File targetFileDir = new File(ExportUtils.getParentPath(targetPath));
+            // 是文件夹就复制所有文件
+            if (realFile.isDirectory()) {
+                targetFile = new File(targetPath);
+                targetFile.mkdirs();
+                // 复制整个文件夹的文件
+                try {
+                    if (FileTest.CLAZZ.equals(FileTest.IMPORT_TYPE)) {
+                        realPath = realPath.replace("src/main/java", "target/classes").replace(".java", ".class");
+                        realFile = new File(realPath);
+                    }
+                    FileUtils.copyDirectoryToDirectory(realFile, targetFileDir);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                if (!targetFileDir.exists()) {
+                    targetFileDir.mkdirs();
+                }
+                // 复制文件
+                try {
+                    if (FileTest.CLAZZ.equals(FileTest.IMPORT_TYPE)) {
+                        realPath = realPath.replace("src/main/java", "target/classes").replace(".java", ".class");
+                        realFile = new File(realPath);
+                        targetPath = targetPath.replace(".java", ".class");
+                        if (realPath.endsWith(".class")) {
+                            // 查找父目录下的文件
+                            List<File> clazzFileList = getClazzFileList(
+                                parentPath.replace("src/main/java", "target/classes"), getFileName(realPath));
+                            for (File file : clazzFileList) {
+                                FileUtils.copyFileToDirectory(file, targetFileDir);
+                                System.out.println(file.getPath());
+                            }
+                            continue;
+                        }
+                    }
+                    FileUtils.copyFileToDirectory(realFile, targetFileDir);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(realPath);
+        }
+    }
+
+    /**
+     * 获取class文件和内部类
+     * 
+     * @param parentPath
+     * @return
+     */
+    public static List<File> getClazzFileList(String parentPath, String fileName) {
+        File file = new File(parentPath);
+        List<File> fileList = Lists.newArrayList();
+        File[] listFiles = file.listFiles();
+        for (File sonFile : listFiles) {
+            String sonFileName = org.springframework.util.StringUtils.delete(sonFile.getName(), ".class");
+            if (sonFileName.startsWith(fileName + "$") || sonFileName.equals(fileName)) {
+                fileList.add(sonFile);
+            }
+        }
+        return fileList;
+    }
+
+    /**
+     * 获取文件名
+     * 
+     * @param path
+     * @return
+     */
+    public static String getFileName(String path) {
+        return path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf("."));
+    }
+
+}
