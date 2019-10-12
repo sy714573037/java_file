052403623484179edff752b1ac01ebe4a6255ddf
sy <sy714573037>
2019-10-12 14:39:14 +0800

    check null

---
+++ src/main/java/com/test/util/CheckNull.java
@@ -0,0 +1,25 @@
+package com.test.util;
+
+import com.google.common.base.Preconditions;
+import org.apache.commons.lang3.Validate;
+import org.springframework.util.Assert;
+
+import java.util.Objects;
+
+public class CheckNull {
+
+    public static void main(String[] args) {
+
+        Object object = new Object();
+        // jdk
+        Objects.requireNonNull(object, "null");
+        // apache
+        Validate.notNull(object, "null");
+        // guava
+        Preconditions.checkNotNull(object, "null");
+        // spring
+        Assert.notNull(object, "null");
+
+    }
+
+}
