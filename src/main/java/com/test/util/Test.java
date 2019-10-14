193f45f66d2502be80119e646147a1d0547b9b08
sy <sy714573037>
2019-10-14 11:19:43 +0800

    优化代码

--- src/main/java/com/test/util/Test.java
+++ src/main/java/com/test/util/Test.java
@@ -1,16 +1,15 @@
 package com.test.util;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-
-import org.apache.commons.io.FileUtils;
-
-import com.google.common.collect.Lists;
+import java.util.stream.Collectors;
 
 /**
  * 文件操作的小例子,与导出java/class文件无关
- * 
+ *
  * @author shuyi
  * @date 2018/10/23
  */
@@ -18,12 +17,8 @@ public class Test {
 
     public static void main(String[] args) throws IOException {
         File file = new File("E:\\t.txt");
-
         List<?> list = FileUtils.readLines(file, "utf-8");
-        List<String> newList = Lists.newArrayList();
-        list.stream().forEach(x -> {
-            newList.add(x + "hello");
-        });
+        List<String> newList = list.stream().map(o -> o + "hello").collect(Collectors.toList());
         FileUtils.writeLines(file, newList);
     }
 
